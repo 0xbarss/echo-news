60a0a3b4a54003c50b6b92d65d6387604a04822a
AtaberkYayla <121246158+AtaberkYayla@users.noreply.github.com>
2024-11-03 14:14:02 +0300

    Homepage widgets are completed

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,11 @@ import 'package:news_api_flutter_package/news_api_flutter_package.dart';
 Future<void> main() async {
   const String apiKey = "a194493e5fd94fff814c7eebf34e2f65";
 
-  NewsAPI newsAPI = NewsAPI(apiKey: apiKey);
-  List<Article> articleList = await newsAPI.getTopHeadlines(country: "us", category: "science");
+  //NewsAPI newsAPI = NewsAPI(apiKey: apiKey);
+  //List<Article> articleList = await newsAPI.getTopHeadlines(country: "us", category: "science");
+  //print(articleList[0].content);
 
-  print(articleList[0].content);
-
-  //runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -40,14 +39,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<String> newsData = [];
   int _bottomNavigationBarIndex = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   void _onSelected(index) {
     setState(() {
@@ -55,25 +48,35 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _displayProfilePage() {
+    setState(() {
+
+    });
+  }
+
+  void _displaySettingsPage() {
+    setState(() {
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Echo News'),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+        leading: IconButton(
+            onPressed: _displayProfilePage, icon: const Icon(Icons.person_2_sharp)),
+        actions: [
+          IconButton(onPressed: _displaySettingsPage, icon: const Icon(Icons.settings))
+        ],
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            IconButton(
-                onPressed: () => {_incrementCounter()},
-                icon: const Icon(Icons.add),
-                color: Colors.deepOrange)
-          ],
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+          },
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
