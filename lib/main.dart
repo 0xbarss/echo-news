43c624515c5e90ca5ebbd1d53a94a0c6a2e994dd
Barış Özdemir <94244632+ngner046@users.noreply.github.com>
2024-11-03 16:00:51 +0300

    Top Headline News added in ListView widget on the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:news_api_flutter_package/model/article.dart';
 import 'package:news_api_flutter_package/news_api_flutter_package.dart';
 
-Future<void> main() async {
-  const String apiKey = "a194493e5fd94fff814c7eebf34e2f65";
-
-  //NewsAPI newsAPI = NewsAPI(apiKey: apiKey);
-  //List<Article> articleList = await newsAPI.getTopHeadlines(country: "us", category: "science");
-  //print(articleList[0].content);
-
+void main() {
   runApp(const MyApp());
 }
 
@@ -39,8 +33,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> newsData = [];
+  final String apiKey = "a194493e5fd94fff814c7eebf34e2f65";
+  late NewsAPI newsAPI;
+
   int _bottomNavigationBarIndex = 0;
+  List<Article> newsData = [];
 
   void _onSelected(index) {
     setState(() {
@@ -48,16 +45,24 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _displayProfilePage() {
-    setState(() {
-
-    });
+  Future<void> _getTopHeadLines(NewsAPI newsAPI) async {
+    try {
+      List<Article> fetchedNewsData = await newsAPI.getTopHeadlines(
+          country: "us", category: "science");
+      setState(() {
+        newsData = fetchedNewsData.where((article) => article.title != "[Removed]").toList();
+      });
+    }
+    catch (e) {
+      print("Error $e");
+    }
   }
 
-  void _displaySettingsPage() {
-    setState(() {
-
-    });
+  @override
+  void initState() {
+    super.initState();
+    NewsAPI newsAPI = NewsAPI(apiKey: apiKey);
+    _getTopHeadLines(newsAPI);
   }
 
   @override
@@ -68,15 +73,28 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.red,
         centerTitle: true,
         leading: IconButton(
-            onPressed: _displayProfilePage, icon: const Icon(Icons.person_2_sharp)),
+            onPressed: () => {}, icon: const Icon(Icons.person_2_sharp)),
         actions: [
-          IconButton(onPressed: _displaySettingsPage, icon: const Icon(Icons.settings))
+          IconButton(onPressed: () => {}, icon: const Icon(Icons.settings))
         ],
       ),
       body: Center(
-        child: ListView.builder(
-          itemBuilder: (BuildContext context, int index) {
-          },
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: newsData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final Article item = newsData[index];
+                  return NewsCard(title: item.title ?? 'No Title',
+                      content: item.content ?? 'No Content',
+                      imageURL: item.urlToImage ?? 'No URL');
+                },
+              ),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -93,6 +111,37 @@ class _MyHomePageState extends State<MyHomePage> {
         currentIndex: _bottomNavigationBarIndex,
         onTap: _onSelected,
         showUnselectedLabels: true,
+      ),
+    );
+  }
+}
+
+class NewsCard extends StatelessWidget {
+  final String title;
+  final String content;
+  final String imageURL;
+
+  const NewsCard({
+    super.key,
+    required this.title,
+    required this.content,
+    required this.imageURL,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(imageURL, width:400, height:200, errorBuilder: (context, object, stackTrace) {
+            return Container(color: Colors.grey, width: 400, height: 200, child: const Center(child: Text("No Image")));
+          }),
+          Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(title))
+        ],
       ),
     );
   }
